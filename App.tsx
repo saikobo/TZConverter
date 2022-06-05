c934e222037efee661f899cbb2afedf64c6dfc4e
Daniel Adepoju <adebalanced02@gmail.com>
2022-06-06 00:15:11 +0100

    :seedling: Initial project setup

---
+++ App.tsx
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+import Home from './modules/Home';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <Home />
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ modules/Home/index.tsx
@@ -0,0 +1,11 @@
+import React, {ReactElement} from 'react';
+import {View, Text} from 'react-native';
+import styles from './styles';
+
+const Home = (): ReactElement => (
+  <View style={styles.container}>
+    <Text>Hello!!!</Text>
+  </View>
+);
+
+export default Home;
